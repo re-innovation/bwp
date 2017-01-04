8f000825e41e43509e06550c805c15314ec92082
Matthew Gates <matthewg42@gmail.com>
2017-01-04 12:00:15 +0000

    rename some functions, better comments

--- test6/main.cpp
+++ test6/main.cpp
@@ -2,23 +2,25 @@
 #include <string.h>
 #include <stdint.h>
 
-#define AUDIO_SYNC_MAXLEN 12
+#define AUDIO_SYNC_BITS 12
 
-uint8_t _buffer[AUDIO_SYNC_MAXLEN];
+// These are global here, but when integrated into firmware will be data
+// members of the AudioMarkReader class, or similar.
+uint8_t _buffer[AUDIO_SYNC_BITS];
 uint8_t _bufPtr = 0;
 
 //! clear the audio mark code buffer
-void reset()
+void resetBuffer()
 {
-    memset(_buffer, 0, sizeof(uint8_t) * AUDIO_SYNC_MAXLEN);
+    memset(_buffer, 0, sizeof(uint8_t) * AUDIO_SYNC_BITS);
     _bufPtr = 0;
 }
 
 //! Append a bit to the audio make buffer
 //! \return true if the bit was appened, else false (i.e. if the buffer if full)
-bool addBit(bool bit) 
+bool appendBitToBuffer(bool bit) 
 {
-    if (_bufPtr >=AUDIO_SYNC_MAXLEN) {
+    if (_bufPtr >=AUDIO_SYNC_BITS) {
         return false;
     } else {
         _buffer[_bufPtr++] = bit;
@@ -33,7 +35,7 @@ bool addBit(bool bit)
 //! Hamming codes use bits which are an integer power of 2 for parity
 //! we add one because we're using zero-indexed array, not 1-indexed.
 //! 
-bool isParityBit(uint8_t bitPos) {
+bool bitIsParityBit(uint8_t bitPos) {
     bitPos++;
     while (((bitPos % 2) == 0) && bitPos > 1) { 
         bitPos /= 2;
@@ -42,11 +44,12 @@ bool isParityBit(uint8_t bitPos) {
 }
 
 //! Check a parity bit in a buffer containing a Hamming code
-//!
+//! \param bitPos the position of the bit in the buffer (zero-indexed)
+//! \return 1 if the bit is a hamming partiy bit, else 0
 uint8_t hammingParity(uint8_t bitPos)
 {   
     // don't worry about non-parity bits...
-    if (!isParityBit(bitPos)) {
+    if (!bitIsParityBit(bitPos)) {
         return 0;
     }
 
@@ -54,7 +57,7 @@ uint8_t hammingParity(uint8_t bitPos)
     uint8_t parity = 0;
     uint8_t left = bitPos+1;
     bool check = true;
-    for (uint8_t i=bitPos; i<AUDIO_SYNC_MAXLEN; i++) {
+    for (uint8_t i=bitPos; i<AUDIO_SYNC_BITS; i++) {
         if (left == 0) {
             left = bitPos+1;
             check = !check;
@@ -67,13 +70,16 @@ uint8_t hammingParity(uint8_t bitPos)
     return parity % 2;
 }
 
-//! Dump the audio mark buffer to stdout - for debugging
-int decode()
+//! Decode the buffer
+//! \return -1 if buffer is incomplete
+//!         -2 on error (too many error bits)
+//!         0-255 on successful decode of 12/8 bit hamming code
+int decodeBuffer()
 {
     uint8_t correction = 0;
     uint8_t bitValue = 1;
-    for (uint8_t i=0; i<AUDIO_SYNC_MAXLEN; i++) {
-        if (isParityBit(i)) {
+    for (uint8_t i=0; i<AUDIO_SYNC_BITS; i++) {
+        if (bitIsParityBit(i)) {
             correction += bitValue * hammingParity(i);
             bitValue *= 2;
         }
@@ -81,11 +87,11 @@ int decode()
                     i, 
                     i<_bufPtr ? "SET" : "unset", 
                     _buffer[i],
-                    isParityBit(i),
+                    bitIsParityBit(i),
                     hammingParity(i),
                     correction);
     }
-    if (correction >= AUDIO_SYNC_MAXLEN) {
+    if (correction >= AUDIO_SYNC_BITS) {
         // More than one error
         printf("ERROR - unfixable amount of errors\n");
         return(-2);
@@ -97,8 +103,8 @@ int decode()
     }
     bitValue = 1;
     uint8_t value = 0;
-    for (uint8_t i=0; i<AUDIO_SYNC_MAXLEN; i++) {
-        if (!isParityBit(i)) {
+    for (uint8_t i=0; i<AUDIO_SYNC_BITS; i++) {
+        if (!bitIsParityBit(i)) {
             value += _buffer[i]*bitValue;
             bitValue *= 2;
         }
@@ -109,9 +115,9 @@ int decode()
 int main(int argc, char** argv)
 {
     for (uint8_t i=1; i<argc; i++) {
-        addBit(argv[i][0] == '0' ? 0 : 1);
+        appendBitToBuffer(argv[i][0] == '0' ? 0 : 1);
     }
-    int v = decode();
+    int v = decodeBuffer();
     printf("value is %d\n", v);
 
     return 0;
