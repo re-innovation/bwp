8ffeb39e53f00057d2394ab672836c2a43a7e05f
Matthew Gates <matthewg42@gmail.com>
2017-01-05 13:55:37 +0000

    MP3-only debugging option

--- test5/Mp3Player.cpp
+++ test5/Mp3Player.cpp
@@ -1,5 +1,7 @@
 #include <DFPlayerMini.h>
 #include <SoftwareSerial.h>
+#include <MutilaDebug.h>
+#include <Arduino.h>
 #include "Mp3Player.h"
 #include "Config.h"
 
@@ -20,6 +22,10 @@ void Mp3Player_::begin()
 
 void Mp3Player_::play(uint16_t trackNumber)
 {
+#ifdef MP3DEBUG
+    Serial.print(F("Mp3Player_::play track="));
+    Serial.println(trackNumber);
+#endif
     _mp3.sendCmd(DFPlayerMini::Play, trackNumber);
 }
 
