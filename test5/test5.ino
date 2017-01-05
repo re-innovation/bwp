095f62687d979cee89d424ae9fbb7b2cb1e6152c
Matthew Gates <matthewg42@gmail.com>
2017-01-05 13:56:55 +0000

    don't forget to init Mp3 device

--- test5/test5.ino
+++ test5/test5.ino
@@ -2,11 +2,13 @@
 
 // Import global objects which go up to make our projector
 #include "Projector.h"
+#include "Mp3Player.h"
 
 void setup()
 {
     Serial.begin(115200);
     Projector.begin();
+    Mp3Player.begin();
     Serial.println(F("E:setup()"));
 }
 
