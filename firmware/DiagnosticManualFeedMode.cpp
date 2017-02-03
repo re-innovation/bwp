76bf1a346327ea2295e1155a51b2f7e7f1d16a5e
Matthew Gates <matthewg42@gmail.com>
2017-02-03 14:25:59 +0000

    reset buttonstate / closeShutter

--- firmware/DiagnosticManualFeedMode.cpp
+++ firmware/DiagnosticManualFeedMode.cpp
@@ -16,7 +16,7 @@ DiagnosticManualFeedMode_::DiagnosticManualFeedMode_()
 void DiagnosticManualFeedMode_::modeStart()
 {
     Projector.setMute(true);
-    Projector.setShutter(false);
+    Projector.closeShutter();
     Mp3Player.play(MP3_TRACK_MANUAL_FEED);
     DBLN(F("DiagnosticManualFeedMode::modeStart"));
 }
@@ -24,6 +24,11 @@ void DiagnosticManualFeedMode_::modeStart()
 void DiagnosticManualFeedMode_::modeStop()
 {
     DBLN(F("DiagnosticManualFeedMode::modeStop()"));
+    // In case the button was pressed, called tappe() to 
+    // collect it's pressed state (else the next read to
+    // tapped in the next more will return true in a 
+    // confusing way!
+    SW3.tapped();
 }
 
 void DiagnosticManualFeedMode_::modeUpdate()
