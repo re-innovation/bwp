7f506441ded442bd6bde15bb982a86ceb1385914
Matthew Gates <matthewg42@gmail.com>
2017-02-06 11:49:43 +0000

    tuning and testing sensors

--- firmware/Config.h
+++ firmware/Config.h
@@ -10,22 +10,22 @@
 #define STEPPER_STEPS_PER_SEC       50
 #define STEPPER_MAX_SPEED           1000
 
-#define SHUTTER_OPEN_MS             200
+#define SHUTTER_OPEN_MS             100
 #define SHUTTER_LED_R_PIN           9
 #define SHUTTER_LED_G_PIN           10
 #define SHUTTER_LED_B_PIN           11
 #define SHUTTER_SYNC_PIN            A3
-#define SHUTTER_SYNC_DIFF_THRESHOLD 300
-#define SHUTTER_SYNC_MIN_STEPS      7
+#define SHUTTER_SYNC_DIFF_THRESHOLD 200
+#define SHUTTER_SYNC_MIN_STEPS      17
 #define SHUTTER_SYNC_MAX_OFFSET     25
 
 #define AUDIO_SYNC_PIN              A4
-#define AUDIO_SYNC_THRESH           390
+#define AUDIO_SYNC_THRESH           400
 #define AUDIO_SYNC_ALPHA            1.0
-#define AUDIO_SYNC_MAX_LEN          30
+#define AUDIO_SYNC_MAX_LEN          35
 #define AUDIO_SYNC_ZERO_MIN_LEN     3
-#define AUDIO_SYNC_ONE_MIN_LEN      9
-#define AUDIO_SYNC_ONE_MAX_LEN      20
+#define AUDIO_SYNC_ONE_MIN_LEN      16
+#define AUDIO_SYNC_ONE_MAX_LEN      25
 #define AUDIO_SYNC_BITS             12
 #define AUDIO_SYNC_INCOMPLETE       -1
 #define AUDIO_SYNC_INVALID          -2
@@ -47,7 +47,7 @@
 #define MP3_RX_PIN                  13
 #define MP3_BUSY_PIN                A0
 #define MP3_POWER_PIN               3
-#define MP3_VOLUME                  3
+#define MP3_VOLUME                  10
 #define MP3_TRACK_NUMBER_OF_USES    190
 #define MP3_TRACK_AVERAGE_RUNTIME   191
 #define MP3_TRACK_TOTAL_RUNTIME     192
