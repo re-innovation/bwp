9ff9e965b86badce996b09f7bcb345108c86c057
Matthew Gates <matthewg42@gmail.com>
2017-02-06 16:19:28 +0000

    more complete runtime

--- firmware/DiagnosticStatSecondsMode.cpp
+++ firmware/DiagnosticStatSecondsMode.cpp
@@ -17,9 +17,21 @@ void DiagnosticStatSecondsMode_::modeStart()
 {
     DBLN(F("DiagnosticStatSecondsMode::modeStart"));
     Mp3Player.appendElement(MP3_TRACK_TOTAL_RUNTIME);
-    unsigned long mins = Settings.getUseSeconds()/60;
-    Mp3Player.readNumber(mins);
-    Mp3Player.appendElement(mins == 1 ? MP3_TRACK_MINUTE : MP3_TRACK_MINUTES);
+    uint16_t hours = Settings.getUseSeconds()/3600;
+    uint16_t mins = (Settings.getUseSeconds()-(hours*3600))/60;
+    uint8_t secs = Settings.getUseSeconds() % 60;
+    if (hours > 0) {
+        Mp3Player.readNumber(hours);
+        Mp3Player.appendElement(hours == 1 ? MP3_TRACK_HOUR : MP3_TRACK_HOURS);
+    }
+    if (mins > 0) {
+        Mp3Player.readNumber(mins);
+        Mp3Player.appendElement(mins == 1 ? MP3_TRACK_MINUTE : MP3_TRACK_MINUTES);
+    }
+    if (secs > 0) {
+        Mp3Player.readNumber(secs);
+        Mp3Player.appendElement(secs == 1 ? MP3_TRACK_SECOND : MP3_TRACK_SECONDS);
+    }
 }
 
 void DiagnosticStatSecondsMode_::modeStop()
