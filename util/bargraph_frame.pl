6d8d0121f7c0336f2a627f90fc27c51fa9fec9cb
Matthew Gates <matthewg42@gmail.com>
2017-06-22 16:16:32 +0100

    show diff as well

--- util/bargraph_frame.pl
+++ util/bargraph_frame.pl
@@ -4,11 +4,14 @@ $MAX = 1024;
 $WIDTH = 150;
 
 while(<>) {
-    if (/sv=(\d+)/) {
+    if (/sv=(\d+).*diff=(\d+)/) {
         $p = $1 / $MAX;
         $l = int($WIDTH * $p);
-        printf("SV: %s %d\n", '#' x $l, $1);
-    } elsif (/(close|open)Shutter/) {
+        $pd = $2 / $MAX;
+        $ld = int($WIDTH * $pd);
+        printf("SV: %s %d\n", '#' x $l,  $1);
+        #printf("DI: %s %d\n", "'" x $ld, $2);
+    } elsif (/openShutter/) {
         print;
     }
 }
